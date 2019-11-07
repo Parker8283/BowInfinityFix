f2f2d2e2992971812bd5d56b26b9b79db1b11339
Parker Young <parker@parker8283.net>
2019-11-06 22:27:29 -0500

    Updated to MC 1.14
    Realized that I don't need to do an ObjectHolder for MC enchantments because the Enchantments class is already ObjectHolder'd. So minor cleanup as well.

--- src/main/java/net/parker8283/bif/BowInfinityFix.java
+++ src/main/java/net/parker8283/bif/BowInfinityFix.java
@@ -1,13 +1,12 @@
 package net.parker8283.bif;
 
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ObjectHolder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,18 +15,15 @@ public class BowInfinityFix {
 
     private static final Logger LOGGER = LogManager.getLogger("BowInfinityFix");
 
-    @ObjectHolder("minecraft:infinity")
-    public static final Enchantment INFINITY = null;
-
     public BowInfinityFix() {
         MinecraftForge.EVENT_BUS.addListener(this::infinityFix);
         LOGGER.info("Fix Registered!");
     }
 
     private void infinityFix(final ArrowNockEvent event) {
-        if (EnchantmentHelper.getEnchantmentLevel(INFINITY, event.getBow()) > 0) {
-            event.getEntityPlayer().setActiveHand(event.getHand());
-            event.setAction(new ActionResult<>(EnumActionResult.SUCCESS, event.getBow()));
+        if (EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, event.getBow()) > 0) {
+            event.getPlayer().setActiveHand(event.getHand());
+            event.setAction(new ActionResult<>(ActionResultType.SUCCESS, event.getBow()));
         }
     }
 }
