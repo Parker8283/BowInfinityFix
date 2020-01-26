c5b722c5d5037dc4152d0790968791683cf227b9
Parker Young <parker@parker8283.net>
2020-01-26 17:10:11 -0500

    Updated to MC 1.15.2

--- src/main/java/net/parker8283/bif/BowInfinityFix.java
+++ src/main/java/net/parker8283/bif/BowInfinityFix.java
@@ -3,7 +3,6 @@ package net.parker8283.bif;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,7 +22,7 @@ public class BowInfinityFix {
     private void infinityFix(final ArrowNockEvent event) {
         if (EnchantmentHelper.getEnchantmentLevel(Enchantments.INFINITY, event.getBow()) > 0) {
             event.getPlayer().setActiveHand(event.getHand());
-            event.setAction(new ActionResult<>(ActionResultType.SUCCESS, event.getBow()));
+            event.setAction(ActionResult.func_226248_a_(event.getBow()));
         }
     }
 }
