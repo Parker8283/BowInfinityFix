fe9d6ee2da071f09f4f76818e1c14f065c83bc6d
Parker Young <parker@parker8283.net>
2024-04-10 22:45:41 -0500

    Auditory compat change, Modrinth URLs.
    * I forgot that I even did this, but I think I have a fix for Auditory not being compatible. But they never updated to 1.20, so I can't test for sure. And I don't feel like going back to 1.19 to implement the change to see. So I'll just commit this for the next version to be ready for that (if it ever happens).
    * I made a Modrinth page for this mod. So add URLs for that, and update the URL for the mod metadata in-game to the Modrinth page. I guess I'm treating that as the main page now.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# [Bow Infinity Fix](https://www.curseforge.com/minecraft/mc-mods/bow-infinity-fix)
+# Bow Infinity Fix
+
+| [Modrinth][1] | [CurseForge][2] |
+|---------------|-----------------|
+| 1.20+         | 1.14+           |
 
 Removes need to have an arrow in your inventory to use the Infinity enchant on your bow.
 
@@ -6,3 +10,6 @@ Removes need to have an arrow in your inventory to use the Infinity enchant on y
 * **Myrathi**: Original InfiniBows mod
 * **Parker8283**: Bow Infinity Fix implementation
 * **MeeniMc**: Part of Fabric 1.17 update
+
+[1]: https://modrinth.com/mod/bow-infinity-fix
+[2]: https://www.curseforge.com/minecraft/mc-mods/bow-infinity-fix
--- src/main/java/net/parker8283/bif/BowInfinityFix.java
+++ src/main/java/net/parker8283/bif/BowInfinityFix.java
@@ -13,8 +13,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BowItem.class)
+@Mixin(value = BowItem.class, priority = BowInfinityFix.PRIORITY)
 public class BowInfinityFix {
+    // To be compatible with the Auditory, make sure our mixin gets applied before it. Its mixin applies on all RETURN
+    // instructions, which our mixin will generate one. So we need to be in before it. Auditory uses the default 1000
+    // priority currently, so we will use 999 for now (I could just do like 1 or something, but that feels wrong).
+    static final int PRIORITY = 999;
+
     @Inject(at = @At("HEAD"), cancellable = true, method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;")
     private void init(Level level, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
         ItemStack stack = player.getItemInHand(hand);
