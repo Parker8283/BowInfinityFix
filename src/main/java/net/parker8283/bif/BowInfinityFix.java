6d720765374f89953140b41beafbb7dd26d924ca
Parker Young <parker@parker8283.net>
2024-05-11 21:20:48 -0500

    Update to MC 1.20.5/6.
    Mojang completely refactored the Bow code and pulled most of the logic into the superclass, which Crossbows also inherit from. Crossbows now support infinity as well. So I moved my patch to be in the Player class, to catch its last return statement, which checks for creative. So I wrap its check with my own for INFINITY. Kinda reminds me of the old Forge coremod, just in a different place now. Upside, don't think I need the Auditory compat anymore, since I'm mixin-ing a different class now.
    
    Also added Parchment mappings. I've meant to do this for a while, but I don't think they've ever had a version released at the time I've done my updates to the mod. Was helpful in understanding the new Mojang code.

--- src/main/java/net/parker8283/bif/BowInfinityFix.java
+++ src/main/java/net/parker8283/bif/BowInfinityFix.java
@@ -1,31 +1,21 @@
 package net.parker8283.bif;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
-import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = BowItem.class, priority = BowInfinityFix.PRIORITY)
+@Mixin(Player.class)
 public class BowInfinityFix {
-    // To be compatible with the Auditory, make sure our mixin gets applied before it. Its mixin applies on all RETURN
-    // instructions, which our mixin will generate one. So we need to be in before it. Auditory uses the default 1000
-    // priority currently, so we will use 999 for now (I could just do like 1 or something, but that feels wrong).
-    static final int PRIORITY = 999;
-
-    @Inject(at = @At("HEAD"), cancellable = true, method = "use(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResultHolder;")
-    private void init(Level level, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
-        ItemStack stack = player.getItemInHand(hand);
-        if (EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, stack) > 0) {
-            player.startUsingItem(hand);
-            cir.setReturnValue(InteractionResultHolder.success(stack));
+    @Inject(at = @At("TAIL"), cancellable = true, method = "getProjectile(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/item/ItemStack;")
+    private void init(ItemStack weaponStack, CallbackInfoReturnable<ItemStack> cir) {
+        if (cir.getReturnValue().isEmpty() && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY, weaponStack) > 0) {
+            cir.setReturnValue(new ItemStack(Items.ARROW));
         }
     }
 }
