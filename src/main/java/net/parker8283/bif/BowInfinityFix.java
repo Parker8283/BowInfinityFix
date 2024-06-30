1a40974eb3da2e2ad36ca9e749aa3ba4cab19ab3
Parker Young <parker@parker8283.net>
2024-06-29 21:48:06 -0500

    Update to MC 1.21
    Mojang has done it again! Enchantment rework made me jostle around in my head about how to do this now. It was between literally checking the Infinity enchantment on the bow/crossbow just as before, or running the `processAmmoUse()` function to see if _any_ of the enchantments on the bow/crossbow reduced the ammo use count to 0. I went with the latter because it seems to offer better mod/datapack compatibility.

--- src/main/java/net/parker8283/bif/BowInfinityFix.java
+++ src/main/java/net/parker8283/bif/BowInfinityFix.java
@@ -1,21 +1,35 @@
 package net.parker8283.bif;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+// We're extending Entity here to get access to the `level()` function.
 @Mixin(Player.class)
-public class BowInfinityFix {
+public abstract class BowInfinityFix extends Entity {
+    // We're required to have a constructor that matches `Entity`s constructor.
+    // But I guess Mixin does some fancy stuff to not need to instantiate this. So we can make it private.
+    private BowInfinityFix(EntityType<?> entityType, Level level) {
+        super(entityType, level);
+    }
+
     @Inject(at = @At("TAIL"), cancellable = true, method = "getProjectile(Lnet/minecraft/world/item/ItemStack;)Lnet/minecraft/world/item/ItemStack;")
     private void init(ItemStack weaponStack, CallbackInfoReturnable<ItemStack> cir) {
-        if (cir.getReturnValue().isEmpty() && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY, weaponStack) > 0) {
-            cir.setReturnValue(new ItemStack(Items.ARROW));
+        if (this.level() instanceof ServerLevel level &&
+            cir.getReturnValue().isEmpty()) {
+            var ret = Items.ARROW.getDefaultInstance();
+            if (EnchantmentHelper.processAmmoUse(level, weaponStack, ret, 1) == 0) {
+                cir.setReturnValue(ret);
+            }
         }
     }
 }
